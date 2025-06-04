5e970902a7f9e4872ecba32ce42db8d7b4f03ae8
Luiz Felipe <fogueteb8@gmail.com>
2025-06-04 09:59:06 -0300

    feat: criando o trigger para validar se a quantidade de itens inseridos em item_venda é condizente com a presente no estoque

--- concessionaria.sql
+++ concessionaria.sql
@@ -285,4 +285,29 @@ $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER VALIDAR_FUNCIONARIO_CLIENTE_TR 
 AFTER INSERT OR UPDATE ON VENDA 
-FOR EACH ROW EXECUTE FUNCTION validar_funcionario_cliente()
+FOR EACH ROW EXECUTE FUNCTION validar_funcionario_cliente();
+
+-- Validar se a quantidade de itens inseridos em item_venda condiz com a presente no estoque 
+
+CREATE OR REPLACE FUNCTION validar_qtd() RETURNS TRIGGER AS 
+$$
+DECLARE 
+	qtd_presente_no_estoque INT;
+BEGIN
+	SELECT C.QTD_EM_ESTOQUE INTO qtd_presente_no_estoque
+	FROM CARRO AS C 
+	JOIN LOJA_CARRO AS LC ON LC.COD_CARRO = C.COD_CARRO
+	WHERE LC.COD_LOJA_CARRO = NEW.COD_LOJA_CARRO;
+
+	IF NEW.QTD_DE_ITENS > qtd_presente_no_estoque THEN
+		RAISE EXCEPTION 'Quantidade de itens (%), maior que a presente no estoque (%)',
+			NEW.QTD_DE_ITENS, qtd_presente_no_estoque;
+	END IF;
+
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER validar_qtd_tr 
+BEFORE INSERT OR UPDATE ON ITEM_VENDA
+FOR EACH ROW EXECUTE FUNCTION validar_qtd();
