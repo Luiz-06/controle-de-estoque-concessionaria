d0f29b8e16867de0d18b74d621b3cb78d167fc84
Luiz Felipe <fogueteb8@gmail.com>
2025-07-02 13:52:36 -0300

    feat: atualizando funcao delete

--- functions/deletar_da_tabela.sql
+++ functions/deletar_da_tabela.sql
@@ -1,15 +1,16 @@
-CREATE OR REPLACE FUNCTION deletar_da_tabela(
-    nome_tabela TEXT,
-    condicao TEXT
-)
+CREATE OR REPLACE FUNCTION deletar_da_tabela(p_nome_tabela TEXT, p_condicao TEXT)
 RETURNS VOID AS $$
 DECLARE
     comando_sql TEXT;
 BEGIN
-    comando_sql := FORMAT('DELETE FROM %s WHERE %s', nome_tabela, condicao);
+    comando_sql := FORMAT('DELETE FROM %I WHERE %s', p_nome_tabela, p_condicao);
+    RAISE NOTICE 'Tentando executar: %', comando_sql;
 
     EXECUTE comando_sql;
+    RAISE NOTICE 'Registo(s) removido(s) com sucesso da tabela %s.', p_nome_tabela;
 
-    RAISE NOTICE 'Comando executado: %', comando_sql;
+EXCEPTION
+    WHEN foreign_key_violation THEN
+        RAISE EXCEPTION 'ERRO: Não é possível remover o registo da tabela "%", pois ele está a ser utilizado por outra tabela. Verifique as associações e tente novamente.', p_nome_tabela;
 END;
 $$ LANGUAGE plpgsql;
