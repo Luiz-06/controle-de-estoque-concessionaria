fa0bdebcf02ae3699f169ab26144904d875c8e01
Luiz Felipe <fogueteb8@gmail.com>
2025-07-05 10:55:18 -0300

    feat: .

--- componentes-do-bd/papeis/funcionario.sql
+++ componentes-do-bd/papeis/funcionario.sql
@@ -4,13 +4,10 @@ GRANT CONNECT ON DATABASE c TO funcionario;
 GRANT USAGE ON SCHEMA public TO funcionario;
 
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO funcionario;
-
-GRANT INSERT ON VENDA, ITEM_VENDA TO funcionario;
-
-GRANT UPDATE ON CARRO, VENDA, FUNCIONARIO, CLIENTE TO funcionario;
-
-GRANT EXECUTE ON FUNCTION fn_realizar_venda(integer, integer) TO funcionario;
-GRANT EXECUTE ON FUNCTION fn_inserir_na_venda(integer, integer, integer) TO funcionario;
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO funcionario;
+REVOKE EXECUTE ON FUNCTION fn_inserir_na_tabela(text, text, text) FROM funcionario;
+REVOKE EXECUTE ON FUNCTION fn_atualizar_tabela(text, text, text) FROM funcionario;
+REVOKE EXECUTE ON FUNCTION fn_deletar_da_tabela(text, text) FROM funcionario;
 
 DO $$
 BEGIN
@@ -21,3 +18,8 @@ END
 $$;
 
 GRANT funcionario TO funcionario_user;
+
+
+
+
+
