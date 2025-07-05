1ad79fd4838503c7c5ab154637a16a71a3f31d32
Luiz Felipe <fogueteb8@gmail.com>
2025-07-05 08:28:50 -0300

    feat: ...

--- componentes-do-bd/papeis/funcionario.sql
+++ componentes-do-bd/papeis/funcionario.sql
@@ -1,21 +1,13 @@
 CREATE ROLE funcionario;
 
-GRANT CONNECT ON DATABASE "nome_do_seu_bd" TO funcionario; 
+GRANT CONNECT ON DATABASE c TO funcionario;
 GRANT USAGE ON SCHEMA public TO funcionario;
 
-GRANT SELECT ON
-   CARRO,
-   CLIENTE,
-   COR,
-   ITEM_VENDA,
-   LOJA,
-   LOJA_CARRO,
-   MARCA,
-   PESSOA_FISICA,
-   PESSOA_JURIDICA,
-   TIPO,
-   VENDA
-TO funcionario;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO funcionario;
+
+GRANT INSERT ON VENDA, ITEM_VENDA TO funcionario;
+
+GRANT UPDATE ON CARRO, VENDA, FUNCIONARIO, CLIENTE TO funcionario;
 
 GRANT EXECUTE ON FUNCTION fn_realizar_venda(integer, integer) TO funcionario;
 GRANT EXECUTE ON FUNCTION fn_inserir_na_venda(integer, integer, integer) TO funcionario;
